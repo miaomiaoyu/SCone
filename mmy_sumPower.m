77ff233000a8362b1189027b255d7061d592dc16
Alex Wade <wade@wadelab.net>
2018-02-07 16:52:52 +0000

    Added a micro edit just to show off git

--- mmy_sumPower.m
+++ mmy_sumPower.m
@@ -8,7 +8,8 @@ function returnedPower=mmy_sumPower(inputSpectrum, freqToSum)
 % But - using 'size' and 'reshape' commands it could easily return the
 % correct answer for any dimensionality of input...
 % 24/03/17, M.Y.
+% 07/02/18 ARW - micro edit
 %
-returnedPower=sum(abs(inputSpectrum([freqToSum] + 1, :)) .^ 2);
+returnedPower=sum(abs(inputSpectrum([freqToSum] + 1, :)) .^2);
 
 return
